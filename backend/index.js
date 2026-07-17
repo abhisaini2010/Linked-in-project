c53443aa493df9e7804799c127435a1cb432f998
abhisaini2010 <abhisaini1101@gmail.com>
2026-07-17 18:33:40 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -22,14 +22,14 @@ let server = http.createServer(app)
 
 export const io = new Server(server,{
     cors:({
-    origin:"http://localhost:5173",
+    origin:"https://linkedin-frontend-rsy3.onrender.com",
     credentials:true
 })
 })
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://linkedin-frontend-rsy3.onrender.com",
     credentials:true
 }))
 app.use("/api/auth",authRouter)
